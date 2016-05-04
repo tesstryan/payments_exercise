a333d0fd552c2de218162c56a20a7b82a16719cf
tesstryan <tesstryan@gmail.com>
2016-05-04 18:36:27 -0500

    Adds callback to set loan amount at creation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,12 @@ class Loan < ActiveRecord::Base
 
   has_many :payments
 
+  before_create :set_outstanding_balance # will initially equal the funded_amount
+
+  private
+
+  def set_outstanding_balance
+    self.outstanding_balance = self.funded_amount
+  end
+
 end
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
+
+  it "has an outstanding_balance when created" do
+    loan = Loan.create!(funded_amount: 200.0)
+    expect(loan.outstanding_balance).to eq(200.0)
+  end
+
 end
